1d34437f55d9cca1a5844a8feefa263a8fc3e421
Rylvion <abdurahmanshari@hotmail.com>
2025-10-08 16:13:57 +0100

    Add comments and script to convert tab-separated ability data into JSON
    - Added detailed comments explaining how to input ability data and format columns.
    - Parses level stats (Pow, Spd, Trick, Recv, Def) from pasted tab-separated data.

--- src/export_data.py
+++ src/export_data.py
@@ -1,32 +1,69 @@
 import json
-# Ability name
-ability_name = "Explosion"
-# Tab-separated data as a multi-line string
+
+ability_name = "Barrier" # Define the ability name
+# Copy and paste the excel data between the triple quotes below with the site below
+# https://docs.google.com/spreadsheets/d/1cCXSA-Qvz8-iB6VP6q1-0MHTFir2HCQrm80RGscqDGk/edit?gid=968418214#gid=968418214
 data_text = """
-3.6	4.6P	1.9S	4.6T	1R	1.9D
-3.7	4.8P	1.9S	4.8T	1R	1.9D
-3.8	4.9P	2.0S	4.9T	1R	2.0D
-3.9	5.1P	2.0S	5.1T	1R	2.0D
-4.0	5.3P	2.1S	5.3T	1R	2.1D
-4.1	5.5P	2.1S	5.5T	1R	2.1D
-4.5	6.3P	2.3S	6.3T	1R	2.3D
-4.9	7.1P	2.5S	7.1T	1R	2.5D
-5.0	7.3P	2.6S	7.3T	1R	2.6D
-5.1	7.5P	2.6S	7.5T	1R	2.6D
-5.2	7.7P	2.7S	7.7T	1R	2.7D
-5.3	7.9P	2.7S	7.9T	1R	2.7D
-5.4	8.1P	2.8S	8.1T	1R	2.8D
-5.5	8.3P	2.8S	8.3T	1R	2.8D
-5.6	8.5P	2.9S	8.5T	1R	2.9D
-5.7	8.7P	2.9S	8.7T	1R	2.9D
-5.8	9.0P	3.0S	9.0T	1R	3.0D
-5.9	9.2P	3.0S	9.2T	1R	3.0D
-6.0	9.4P	3.1S	9.4T	1R	3.1D
-6.1	9.6P	3.2S	9.6T	1R	3.2D
-6.2	9.8P	3.2S	9.8T	1R	3.2D
-6.3	10P	3.3S	10T	1R	3.3D
-6.4	10P	3.6S	10T	1R	3.6D
-"""
+6.0	7.2P	1S	5.2T	2.6R	10.0D
+6.1	7.5P	1S	5.3T	2.6R	10.0D
+6.2	7.8P	1S	5.5T	2.7R	10.0D
+6.3	8.1P	1S	5.7T	2.8R	10.0D
+6.4	8.4P	1S	5.9T	2.8R	10.0D
+6.5	8.7P	1S	6.1T	2.9R	10.0D
+6.6	9.0P	1S	6.3T	3.0R	10.0D
+6.7	9.3P	1S	6.5T	3.1R	10.0D
+6.8	9.6P	1S	6.7T	3.1R	10.0D
+6.9	9.9P	1S	6.9T	3.2R	10.0D
+7.0	10.0P	1S	7.2T	3.3R	10.0D
+7.1	10.0P	1S	7.6T	3.5R	10.0D
+7.2	10.0P	1S	8.1T	3.7R	10.0D
+7.3	10.0P	1S	8.5T	3.8R	10.0D
+7.4	10.0P	1S	8.9T	4.0R	10.0D
+7.5	10.0P	1S	9.3T	4.1R	10.0D
+7.6	10.0P	1S	9.8T	4.3R	10.0D
+7.7	10.0P	1S	10.0T	4.7R	10.0D
+7.8	10.0P	1S	10.0T	5.3R	10.0D
+7.9	10.0P	1S	10.0T	5.9R	10.0D
+8.0	10.0P	1S	10.0T	6.5R	10.0D
+""" # copy and paste excel data here the six columns are level, Pow, Spd, Trick, Recv, Def make sure to only copy that and nothing else
+# make sure to use tabs to separate the columns but when pasting excel data it should automatically do that
+# if there is any missing data just delete the entire row from the data_text variable above or any quesiton marks within the data variable
+
+############################ EXPECTED OUTPUT FROM pydump.json ############################
+# (with different numbers of values depending on the data above)
+# {
+#   "Arachnid": {
+#     "5.0": {"Pow": 4.1,"Spd": 4.1,"Trick": 4.1,"Recv": 2.0,"Def": 6.2},
+#     "5.1": {"Pow": 4.2,"Spd": 4.2,"Trick": 4.2,"Recv": 2.1,"Def": 6.4},
+#     "5.2": {"Pow": 4.3,"Spd": 4.3,"Trick": 4.3,"Recv": 2.1,"Def": 6.6},
+#     "5.3": {"Pow": 4.4,"Spd": 4.4,"Trick": 4.4,"Recv": 2.1,"Def": 6.7},
+#     "5.4": {"Pow": 4.5,"Spd": 4.5,"Trick": 4.5,"Recv": 2.2,"Def": 6.9},
+#     "5.5": {"Pow": 4.7,"Spd": 4.7,"Trick": 4.7,"Recv": 2.2,"Def": 7.1},
+#     "5.6": {"Pow": 4.8,"Spd": 4.8,"Trick": 4.8,"Recv": 2.3,"Def": 7.3},
+#     "5.7": {"Pow": 4.9,"Spd": 4.9,"Trick": 4.9,"Recv": 2.3,"Def": 7.4},
+#     "5.8": {"Pow": 5.0,"Spd": 5.0,"Trick": 5.0,"Recv": 2.3,"Def": 7.6},
+#     "5.9": {"Pow": 5.1,"Spd": 5.1,"Trick": 5.1,"Recv": 2.4,"Def": 7.8},
+#     "6.0": {"Pow": 5.2,"Spd": 5.2,"Trick": 5.2,"Recv": 2.4,"Def": 8.0},
+#     "6.1": {"Pow": 5.3,"Spd": 5.3,"Trick": 5.3,"Recv": 2.4,"Def": 8.2},
+#     "6.2": {"Pow": 5.4,"Spd": 5.4,"Trick": 5.4,"Recv": 2.5,"Def": 8.4},
+#     "6.3": {"Pow": 5.5,"Spd": 5.5,"Trick": 5.5,"Recv": 2.5,"Def": 8.5},
+#     "6.4": {"Pow": 5.6,"Spd": 5.6,"Trick": 5.6,"Recv": 2.5,"Def": 8.7},
+#     "6.5": {"Pow": 5.7,"Spd": 5.7,"Trick": 5.7,"Recv": 2.6,"Def": 8.9},
+#     "6.6": {"Pow": 5.9,"Spd": 5.9,"Trick": 5.9,"Recv": 2.6,"Def": 9.1},
+#     "6.7": {"Pow": 6.0,"Spd": 6.0,"Trick": 6.0,"Recv": 2.7,"Def": 9.3},
+#     "6.8": {"Pow": 6.1,"Spd": 6.1,"Trick": 6.1,"Recv": 2.7,"Def": 9.5},
+#     "6.9": {"Pow": 6.2,"Spd": 6.2,"Trick": 6.2,"Recv": 2.7,"Def": 9.7},
+#     "7.0": {"Pow": 6.3,"Spd": 6.3,"Trick": 6.3,"Recv": 2.8,"Def": 9.9},
+#     "7.1": {"Pow": 6.4,"Spd": 6.4,"Trick": 6.4,"Recv": 2.8,"Def": 10.0},
+#     "7.2": {"Pow": 6.6,"Spd": 6.6,"Trick": 6.6,"Recv": 2.9,"Def": 10.0},
+#     "7.3": {"Pow": 6.8,"Spd": 6.8,"Trick": 6.8,"Recv": 2.9,"Def": 10.0},
+#     "7.4": {"Pow": 7.0,"Spd": 7.0,"Trick": 7.0,"Recv": 3.0,"Def": 10.0},
+#     "7.5": {"Pow": 7.1,"Spd": 7.1,"Trick": 7.1,"Recv": 3.0,"Def": 10.0},
+#     "7.6": {"Pow": 7.3,"Spd": 7.3,"Trick": 7.3,"Recv": 3.1,"Def": 10.0}
+#   }
+# }
+
+############### IGNORE BELOW THIS LINE UNLESS YOU KNOW EXACTLY WHAT YOU'RE DOING ###############
 
 abilities = {}
 
